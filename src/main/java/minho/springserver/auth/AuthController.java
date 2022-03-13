a0f4e6aa3e072aa3c8ea470e47fdb1fe354cadc0
kwon, minho <minhob38@gmail.com>
2022-03-13 17:12:18 +0900

    add header controller

--- src/main/java/minho/springserver/auth/AuthController.java
+++ src/main/java/minho/springserver/auth/AuthController.java
@@ -2,12 +2,19 @@ package minho.springserver.auth;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class AuthController {
     private final Logger log = LoggerFactory.getLogger((getClass()));
 
+    /* api https://docs.spring.io/spring-framework/docs/current/reference/html/web.html#mvc-ann-methods */
     @RequestMapping(value = "/api/auth/signup", method = RequestMethod.POST)
     public String postSignUp() {
         String name = "Spring";
@@ -42,4 +49,23 @@ public class AuthController {
         return "ok";
     }
 
+
+    @GetMapping(value = "/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @RequestHeader("host") String host,
+                          @CookieValue(value = "myCookie", required = false) String cookie
+
+    )  {
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("headerMap={}", headerMap);
+        log.info("host={}", host);
+        log.info("cookie={}", cookie);
+
+        return "ok";
+    }
 }
