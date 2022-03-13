8d354e46cc6e0cbde4b2b4e32c61e7b39d0676de
kwon, minho <minhob38@gmail.com>
2022-03-13 16:46:52 +0900

    implement url mapping

--- src/main/java/minho/springserver/auth/AuthController.java
+++ src/main/java/minho/springserver/auth/AuthController.java
@@ -8,8 +8,29 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final Logger log = LoggerFactory.getLogger((getClass()));
 
-    @RequestMapping(value = "/api/auth/signup", method = RequestMethod.POST) //@PostMappring(value="/")
-    public String logTest() {
+    @RequestMapping(value = "/api/auth/signup", method = RequestMethod.POST)
+    public String postSignUp() {
+        String name = "Spring";
+        log.info("info log={}", name);
+        return "ok" ;
+    }
+
+    @PostMapping(value = "/api/auth/signin")
+    public String postSignIn() {
+        String name = "Spring";
+        log.info("info log={}", name);
+        return "ok" ;
+    }
+
+    @PatchMapping(value = "/api/auth/password")
+    public String patchPassword() {
+        String name = "Spring";
+        log.info("info log={}", name);
+        return "ok" ;
+    }
+
+    @DeleteMapping(value = "/api/auth/signout")
+    public String deleteSignOut() {
         String name = "Spring";
         log.info("info log={}", name);
         return "ok" ;
