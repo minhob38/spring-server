6b539a36dc7b2bdeb5c193d52d27d8a262d27b34
minho <minhob38@gmail.com>
2023-05-27 21:00:57 +0900

    implement seller entity

---
+++ src/main/java/minho/springserver/api/domain/seller/entity/BaseEntity.java
@@ -0,0 +1,19 @@
+package minho.springserver.api.domain.seller.entity;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@MappedSuperclass
+//@EntityListeners(AuditingEntityListener.class)?
+public abstract class BaseEntity {
+    @CreationTimestamp
+    private ZonedDateTime createdAt;
+
+    @UpdateTimestamp
+    private ZonedDateTime updatedAt;
+}
---
+++ src/main/java/minho/springserver/api/domain/seller/entity/Sellers.java
@@ -0,0 +1,55 @@
+package minho.springserver.api.domain.seller.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+
+@Getter
+// @Setter data integrity를 위해 setter를 사용하지 않는게 좋으며, 따라서 생성자에서 member들을 할당해줍니다.
+@Entity
+@NoArgsConstructor
+@Table(name = "sellers", schema = "public")
+public class Sellers extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sellerName;
+    private String businessNumber;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @RequiredArgsConstructor
+    public enum Status {
+        ENABLED("활성"), DISABLED("비활성");
+        private final String description;
+    }
+
+    @Builder
+    public Sellers(String sellerName, String businessNumber, String email) {
+        if (StringUtils.isEmpty(sellerName)) throw new RuntimeException("no seller name");
+        if (StringUtils.isEmpty(businessNumber)) throw new RuntimeException("no business number");
+        if (StringUtils.isEmpty(email)) throw new RuntimeException("no email");
+
+        this.sellerName = sellerName;
+        this.businessNumber = businessNumber;
+        this.email = email;
+        this.status = Status.ENABLED;
+    }
+
+    public void enable() {
+        this.status = Status.ENABLED;
+    }
+
+    public void disable() {
+        this.status = Status.DISABLED;
+    }
+}
